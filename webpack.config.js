be8445ee4e0bf046901c6863c7d4ac63150c1791
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2022-08-09 13:22:33 -0700

    Webpack setup not including node modules

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { 
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+};
